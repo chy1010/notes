44f936066afd30e8ac47df6418cceca44573fc1e
chy1010 <andy1010@gmail.com>
2021-12-09 03:16:13 +0800

    modify the suppresion procedure

--- python-hacks/bbox-tools/bbox_nms.py
+++ python-hacks/bbox-tools/bbox_nms.py
@@ -26,17 +26,25 @@ def np_bbox_nms(bboxes: Union[np.ndarray, List[np.ndarray]],
     scores = scores.copy()[score_rankings]
 
     ious = multi_bboxes_iou(bboxes, bboxes.copy()) - np.eye(len(bboxes))
-    poped_inds = set()
-    selected_inds = []
-    for ind, iou in enumerate(ious):
-        if ind in poped_inds: continue
 
-        overlap_inds = np.where(iou > iou_threshold)
-        assert len(overlap_inds) <= 1
-        overlap_inds = next(iter(overlap_inds))
-        for j in overlap_inds:
-            poped_inds.add(j)
-        selected_inds.append(ind)
+    poped_inds = set()
+    rows, columns = np.where(ious > iou_threshold)
+    for row, col in zip(rows, columns):
+        if row in poped_inds: continue
+        poped_inds.add(col)
+    selected_inds = list(set(range(len(ious))) - poped_inds)
+
+    # poped_inds = set()
+    # selected_inds = []
+    # for ind, iou in enumerate(ious):
+    #     if ind in poped_inds: continue
+
+    #     overlap_inds = np.where(iou > iou_threshold)
+    #     assert len(overlap_inds) <= 1
+    #     overlap_inds = next(iter(overlap_inds))
+    #     for j in overlap_inds:
+    #         poped_inds.add(j)
+    #     selected_inds.append(ind)
 
     return score_rankings[np.asarray(selected_inds)]
 
@@ -45,19 +53,22 @@ if __name__ == '__main__':
     N = 100
     iou_threshold = 0.4
 
-    bboxes = torch.Tensor([generate_bbox(low=0, high=50, format='xyxy') for _ in range(N)])
-    scores = torch.Tensor([np.random.random() for _ in range(N)])
+    bboxes = [generate_bbox(low=0, high=50, format='xyxy') for _ in range(N)]
+    scores = [np.random.random() for _ in range(N)]
+
+    np_bboxes = np.asarray(bboxes)
+    np_scores = np.asarray(scores)
 
     from time import time
-
     start_time = time()
-    nms_result = nms(boxes=bboxes, scores=scores, iou_threshold=iou_threshold)
-    print(f'torchvision nms takes: {time() - start_time} seconds.')
-    nms_result = nms_result.numpy()
-
-    bboxes = bboxes.numpy()
-    scores = scores.numpy()
-
-    start_time = time()
-    np_nms_result = np_bbox_nms(bboxes, scores, iou_threshold=iou_threshold)
+    np_nms_result = np_bbox_nms(np_bboxes, np_scores, iou_threshold=iou_threshold)
     print(f' my np bbox nms takes: {time() - start_time} seconds.')
+
+    t_bboxes = torch.Tensor(bboxes)
+    t_scores = torch.Tensor(scores)
+
+    start_time = time()
+    nms_result = nms(boxes=t_bboxes, scores=t_scores, iou_threshold=iou_threshold)
+    nms_result = nms_result.numpy()
+    print(f'torchvision nms takes: {time() - start_time} seconds.')
+
