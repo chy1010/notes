7563ba06cd7d9b4fb2cc1813c64d1f2f819bac13
chy1010 <andy1010@gmail.com>
2021-12-22 03:02:43 +0800

    add most voted

---
+++ leetcode/recycle-codes/med_236/most-voted.py
@@ -0,0 +1,12 @@
+def lowestCommonAncestor(self, root, p, q):
+    if root in (None, p, q): return root
+    left, right = (self.lowestCommonAncestor(kid, p, q)
+                   for kid in (root.left, root.right))
+    return root if left and right else left or right
+
+# Or using that None is considered smaller than any node:
+# def lowestCommonAncestor(self, root, p, q):
+#     if root in (None, p, q): return root
+#     subs = [self.lowestCommonAncestor(kid, p, q)
+#             for kid in (root.left, root.right)]
+#     return root if all(subs) else max(subs)
