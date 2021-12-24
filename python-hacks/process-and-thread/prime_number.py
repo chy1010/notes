cc565d47c33c1d06ee84bb4a12de498bcc031f05
chy1010 <andy1010@gmail.com>
2021-12-25 03:34:53 +0800

    know how join works

--- python-hacks/process-and-thread/prime_number.py
+++ python-hacks/process-and-thread/prime_number.py
@@ -1,31 +1,42 @@
 """
 refer: https://github.com/arifulhaqueuc/python-multithreading-examples
 """
-
-import threading 
+import threading
 import numpy as np
- 
-class PrimeNumber(threading.Thread): 
-    def __init__(self, number): 
-        threading.Thread.__init__(self) 
+
+
+class PrimeNumber(threading.Thread):
+    def __init__(self, number):
+        threading.Thread.__init__(self)
         self.number = number
- 
-    def run(self): 
-        counter = 2 
-        while counter * counter < self.number: 
+
+    def run(self):
+        if self.number % 2 == 0:
+            print(f'{self.number:-8d} is not a prime number:',
+                  f'{self.number:-8d} = 2*{self.number // 2}')
+            return
+        counter = 3
+        while counter * counter < self.number:
             if self.number % counter == 0:
-                print(f'{self.number:-8d} is not a prime number: {self.number:-8d} = {counter}*{self.number // counter}') 
-                return 
-            counter += 1 
+                print(
+                    f'{self.number:-8d} is not a prime number:',
+                    f'{self.number:-8d} = {counter}*{self.number // counter}')
+                return
+            counter += 2
         print(f'{self.number:-8d} is a prime number.')
 
+
 if __name__ == '__main__':
-    threads = [] 
-    inputs = np.random.randint(low=2, high=int(1e8), size=10).tolist()
+    threads = []
+    inputs = np.random.randint(low=2, high=int(1e8), size=10000).tolist()
+    inputs = [6 * input + 1 for input in inputs]
     for input in inputs:
-        thread = PrimeNumber(input) 
-        threads += [thread] 
-        thread.start() 
-    
-    for x in threads: 
-        x.join()
+        thread = PrimeNumber(input)
+        threads += [thread]
+        thread.start()
+
+    # for x in threads:
+    #     x.join()
+
+    print(f'Join: wait for all thread endding. Then continue.')
+    print(f'If no join, this line may be printed before all threads end.')
