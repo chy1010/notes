1835c4c179010c736f57c3ab5f28f7cb8f90ec45
chy1010 <andy1010@gmail.com>
2021-12-31 00:43:21 +0800

    update common use function

--- common/test_cases.py
+++ common/test_cases.py
@@ -1,3 +1,5 @@
+from functools import reduce
+
 def is_prime(input: int) -> bool:
     if input % 2 == 0:
         return False
@@ -7,3 +9,12 @@ def is_prime(input: int) -> bool:
             return False
         counter += 2
     return True
+
+def str2int(s: str):
+    DIGITS = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}
+    def fn(x,y):
+        return 10*x+y
+    def char2digit(s: str):
+        assert s in DIGITS, f'{s} is not a digit.'
+        return DIGITS[s]
+    return reduce(fn, map(char2digit, s))
