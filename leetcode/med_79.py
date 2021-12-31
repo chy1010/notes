18e175a466b863fdaba6886c5b2751d58f200e96
chy1010 <andy1010@gmail.com>
2022-01-01 03:06:34 +0800

    med_79: separated into subsequence

--- leetcode/med_79.py
+++ leetcode/med_79.py
@@ -7,59 +7,82 @@ horizontally or vertically neighboring. The same letter cell may not be used mor
 from collections import defaultdict
 from typing import List
 from copy import deepcopy
+from itertools import product
+import numpy as np
 
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         
         m = len(board)
         n = len(next(iter(board)))
-        
-        counts = defaultdict(int)
-        for c in word:
-            counts[c]+=1
+        self.table = np.zeros(shape=(m, n))
             
         coord_table = defaultdict(list)
         for i, row in enumerate(board):
             for j, c in enumerate(row):
-                coord_table[c].append((i,j))
-        # coord_table = dict(coord_table)
-        # print(counts)
-        # for c in coord_table:
-        #     print(f'{c}: {len(coord_table[c])}')
-                
-        if not all([ repeats <= len(coord_table[c]) for c, repeats in counts.items()]):
+                coord_table[c].append((i,j))        
+        self.coord_table = coord_table
+        
+        is_exist, merged_chains = self.check_exist(word = word)
+        if not is_exist:
             return False
         
-        return self.chess_move(remain_chars=word, chars_coord_table=coord_table)
+        for possible_coords in merged_chains:
+            table = deepcopy(self.table)
+            fail = False
+            for coord in possible_coords:
+                if coord is None:
+                    continue
+                if table[coord] > 0:
+                    fail = True
+                    break
+                table[coord] += 1
+            if not fail:
+                return True
+        return False
+    
+    def check_exist(self, word):    
+        if len(word) == 0:
+            return True, [[None]]
+        elif len(word) == 1:
+            return True, [ [coord] for coord in self.coord_table[word]]
         
-    def chess_move(self, remain_chars, chars_coord_table, cursor = None):
-        """[summary]
-        Args:
-            remain_chars (str): remaining characters need to find on the table
-            chars_coord_table (dict): positions of the remain characters
-            cursor (tuple):
-                The position of the last found char.
-                If None, now is finding the first char.
-        Returns:
-            If the first remaining char is found and is the neighbor of the previous char.
-            Then check the next step.
-        """
-        c = remain_chars[0]
-        # print(f'Finding {c}.')
-        # print(f'next_move: {chars_coord_table[c]}')
-        for next_move in chars_coord_table[c]:
-            # if cursor is None: this is the first char to find.
-            if self.check_neighbor(cursor, next_move):
-                if len(remain_chars) == 1:
-                    return True
-                next_coord_table = deepcopy(chars_coord_table)
-                next_coord_table[c].remove(next_move)
-                
-                if self.chess_move(remain_chars=remain_chars[1:], chars_coord_table=next_coord_table, cursor=next_move):
-                    return True
-        return False            
-                    
-                
+        length = len(word)
+        half = length // 2
+        left = word[:half]
+        right = word[half:]
+        
+        is_left_exists, left_chains = self.check_exist(left)
+        is_right_exists, right_chains = self.check_exist(right)
+            
+        if not (is_left_exists and is_right_exists):
+            return False, [[]]
+        
+        merged_chains = []
+        for left_chain, right_chain in product(left_chains, right_chains):
+            if self.check_neighbor(left_chain[-1], right_chain[0]):
+                table = deepcopy(self.table)
+                fail = False
+                for coord in left_chain:
+                    if coord is None:
+                        continue
+                    if table[coord] > 0:
+                        fail = True
+                        break
+                    table[coord] += 1
+                if fail:
+                    continue
+                for coord in right_chain:
+                    if table[coord] > 0:
+                        fail = True
+                        break
+                    table[coord] += 1
+                if fail:
+                    continue
+                merged_chain = right_chain if left_chain[0] is None else left_chain + right_chain
+                merged_chains.append(merged_chain)
+        return (len(merged_chains) > 0), merged_chains
+
     @staticmethod
     def check_neighbor(coord_1, coord_2):
         if coord_1 is None or coord_2 is None:
@@ -75,17 +98,27 @@ if __name__ == '__main__':
     board = [['A', 'B', 'C', 'E'], ['S', 'F', 'C', 'S'], ['A', 'D', 'E', 'E']]
     word = 'ABCC'
     
-    board = [['a']]
-    word = 'ab'
+    # board = [['a']]
+    # word = 'ab'
     
-    board = [["A","A","A","A","A","A"],["A","A","A","A","A","A"],["A","A","A","A","A","A"],["A","A","A","A","A","A"],["A","A","A","A","A","B"],["A","A","A","A","B","A"]]
-    word = "AAAAAAAAAAAAABB"
+    # board = [["A","A","A","A","A","A"],["A","A","A","A","A","A"],["A","A","A","A","A","A"],["A","A","A","A","A","A"],["A","A","A","A","A","B"],["A","A","A","A","B","A"]]
+    # word = "AAAAAAAAAAAAABB"
+    
+    board = [["B","B","B","B"],["B","B","B","B"],["B","C","B","B"],["B","B","B","B"],["B","B","B","B"]]
+    word = "BBBBBBBBBBBBBCB"
+    
+    # board = [["A","A","A","A","A","A"],["A","A","A","A","A","A"],["A","A","A","A","A","A"],["A","A","A","A","A","A"],["A","A","A","A","A","B"],["A","A","A","A","B","A"]]
+    # word = "BBAAAAAAAAAAAAA"
     
     # print(f'board = {board}')
     for row in board:
         for c in row:
             print(c, end=' ')
         print('')
-    print(f'word  = {word}')
+    print(f'word = {word}')
+    print('-=-'*30)
     sol = Solution()
+    from time import time
+    start_time = time()
     print(sol.exist(board=board, word=word))
+    print(f'duration time: {time() - start_time}')
