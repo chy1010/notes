c17d585507476d9433f1cb711957ec179b0cc5a6
chy1010 <andy1010@gmail.com>
2022-01-17 04:25:12 +0800

    try c

---
+++ c-and-cpp/beginner/hello_world.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void){
+
+    printf("Hello World!\n");
+
+    return 0;
+}
+
+// https://www.foolegg.com/how-to-compile-a-helloworld-program-using-c-in-ubuntu/
+// gcc -g -Wall c-and-cpp/beginner/hello_world.c -o ctemp/HelloWorld
+// ctemp/HelloWorld
