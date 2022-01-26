70c7723fe17e25031e906c6fec1eaf7b9045e74a
chy1010 <andy1010@gmail.com>
2022-01-27 02:16:43 +0800

    change between char and ascii code

---
+++ python-hacks/string/ascii_code.py
@@ -0,0 +1,14 @@
+# >>> ord('a')
+# 97
+# >>> chr(97)
+# 'a'
+# >>> chr(ord('a') + 3)
+# 'd'
+# >>>
+
+
+# ord changes char to int
+# chr changes int to char
+
+print(f'a is mapped to the ordinal number: {ord("a")}.')
+print(f'the 97 is mapped to {chr(97)}.')
