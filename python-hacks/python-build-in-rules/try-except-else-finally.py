47db49616fb745c4a58c7abaff40ee0c127f578c
chy1010 <andy1010@gmail.com>
2022-02-01 23:47:27 +0800

    finally

--- python-hacks/python-build-in-rules/try-except-else-finally.py
+++ python-hacks/python-build-in-rules/try-except-else-finally.py
@@ -1,6 +1,4 @@
 
-
-
 for i in range(10):
     try:
         assert i < 5
@@ -8,3 +6,9 @@ for i in range(10):
         print("Oh no! An exception was raised.")
     else:
         print("Oh good, no exceptions were raised.")
+
+    # usage of finally is optional.
+    finally:
+        print(f'Round {i} is done.', end='; ')
+
+    print(f'Yes, Round {i} is done.')
